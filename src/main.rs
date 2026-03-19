5ac9b0ae6a3728a08dffa4e17f0b1527b30c342e
Aditya Srinivas Manohar <adityasrinivasmanohar@gmail.com>
2026-03-18 20:37:48 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Util to provide info on a given sqlite database
+    Info { 
+        /// Path to the sqlite database file
+        path: String
+    },
+
+    /// Merge multiple sqlite databases into one
+    Merge{
+        /// Paths to the sqlite database files to merge
+        paths: Vec<String>,
+
+        /// Output path for the merged database
+        #[arg(short, long)] // This makes it -o or --output
+        output: String,
+    },
+
+    /// Version
+    Version,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Info { path } => {
+            println!("You want to inspect: {}", path);
+        }
+        Commands::Merge { paths, output } => {
+            println!("Merging databases: {:?} into {}", paths, output);
+        }
+        Commands::Version => {
+            let version = env!("CARGO_PKG_VERSION");
+            println!("db-cli {}", version);
+        }
+    }
+}
